5e5ca12bf1937a931c98d1d0a6286809c472708f
Rutger <1201954@student.roc-nijmegen.nl>
2024-04-05 15:03:12 +0200

    Create header hover animation and active state when on that page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 export default function Home() {
   return (
-    <main className="row-auto flex-col items-center justify-between p-24"></main>
+    <main className="row-auto flex-col items-center justify-between p-24">
+      <h1>Homepage</h1>
+    </main>
   );
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,28 +1,29 @@
 import Link from "next/link";
 import Image from "next/image";
+import HeaderLink from "@/components/HeaderLink";
 
 export default function Header() {
   return (
     <header
       className={
-        "flex h-20 items-center justify-between bg-primary px-10 shadow-standard"
+        "flex h-16 items-center justify-between bg-primary px-10 shadow-standard"
       }
     >
       <Link href={"/"}>
         <Image
-          height={38.4}
-          width={199.2}
+          height={32}
+          width={166}
           src={"/thuisbezorgd.webp"}
           alt={"Thuisbezorgd logo"}
         />
       </Link>
 
       <div className={"flex items-center gap-x-5"}>
-        <Link href={"/menu"}>Menu</Link>
-        <Link href={"/reserveren"}>Reserveren</Link>
-        <Link href={"/contact"}>Contact</Link>
-        <Link href={"/account"}>Account</Link>
-        <Link href={"/admin"}>Admin</Link>
+        <HeaderLink href={"/menu"}>Menu</HeaderLink>
+        <HeaderLink href={"/reserveren"}>Reserveren</HeaderLink>
+        <HeaderLink href={"/contact"}>Contact</HeaderLink>
+        <HeaderLink href={"/account"}>Account</HeaderLink>
+        <HeaderLink href={"/admin"}>Admin</HeaderLink>
       </div>
     </header>
   );
