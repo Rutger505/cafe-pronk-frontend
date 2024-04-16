b6eadecfb726a3757886892041250f09a6276a36
Rutger <rutger.pronk11@gmail.com>
2024-04-16 21:15:01 +0200

    Page account overview anonymous

--- src/app/account/page.tsx
+++ src/app/account/page.tsx
@@ -6,17 +6,17 @@ export default function AccountOverview() {
     <main className={"flex flex-col items-center"}>
       <h1 className={"mb-10 text-xl "}>Accountoverzicht</h1>
       <div className={"flex w-full flex-col items-start"}>
-        <h2 className={"mb-5 text-lg"}>Welkom Rutger</h2>
+        <h2 className={"mb-5 text-lg"}>Welkom &lt;user name&gt;</h2>
         <form className={"mt-8 grid grid-cols-2 grid-rows-2 gap-x-4"}>
           <div>
-            <Input label={"Naam*"} id={"name"} value={"Rutger"} required />
+            <Input label={"Naam*"} id={"name"} value={"<user name>"} required />
           </div>
           <div>
             <Input
               label={"E-mailadres*"}
               id={"email"}
               type={"email"}
-              value={"rutger@email.com"}
+              value={"<user email>"}
               required
             />
           </div>
