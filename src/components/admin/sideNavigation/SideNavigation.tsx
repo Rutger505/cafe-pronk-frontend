7e7dd1495843bae27ef8d2dc3c1bb6af18ce7f19
Rutger <1201954@student.roc-nijmegen.nl>
2024-04-18 13:22:39 +0200

    Change side navigation order

--- src/components/admin/sideNavigation/SideNavigation.tsx
+++ src/components/admin/sideNavigation/SideNavigation.tsx
@@ -4,14 +4,14 @@ export default function SideNavigation() {
   return (
     <nav className="ml-auto mt-32 flex w-fit flex-col">
       <SideNavigationLink href="/admin">Dashboard</SideNavigationLink>
-      <SideNavigationLink href="/admin/contactMessages">
-        Contact berichten
-      </SideNavigationLink>
-      <SideNavigationLink href="/admin/menu">Menu beheren</SideNavigationLink>
       <SideNavigationLink href="/admin/reservations">
         Reservaties beheren
       </SideNavigationLink>
       <SideNavigationLink href="/admin/contactMessages">
+        Contact berichten
+      </SideNavigationLink>
+      <SideNavigationLink href="/admin/menu">Menu beheren</SideNavigationLink>
+      <SideNavigationLink href="/admin/manage-admins">
         Admins beheren
       </SideNavigationLink>
     </nav>
